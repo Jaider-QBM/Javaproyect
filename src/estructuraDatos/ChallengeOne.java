a315127e74b8ae8e7e4b2e0207ab2012b39da863
Jaider-QBM <jaiderstivenquimbaya9@gmail.com>
2023-02-28 11:59:33 -0500

    Reto1

---
+++ src/estructuraDatos/ChallengeOne.java
@@ -0,0 +1,41 @@
+package estructuraDatos;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class ChallengeOne {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        double promedio, resultado, suma;
+
+        int cantidad;
+        resultado =0;
+        suma =0;
+
+        System.out.println("Digite la cantidad de notas");
+        cantidad = lectura.nextInt();
+
+        int[] nota = new int[cantidad];
+        
+        for(int p=0; p<nota.length; p++){
+            System.out.println("Digite las notas de dicha materia ");
+            nota[p] = lectura.nextInt();
+
+            suma = suma + nota[p];
+        }
+
+        promedio = suma / nota.length;
+        resultado = resultado + promedio;
+        if(resultado<=3.0){
+            System.out.println("Reprobaste, el promedio fue de: " + promedio );
+        }
+        else if(resultado<=4.0){
+            System.out.println("Pasaste Raspando, el promedio fue de: " + promedio);
+        }
+        else if(resultado>=4.0){
+            System.out.println("Aprobaste con buenos resultados, el promedio fue de: " + promedio);
+        }
+
+        lectura.close();
+    }
+}
